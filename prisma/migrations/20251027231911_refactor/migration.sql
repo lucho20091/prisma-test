82afe16a7b19fa6a129f95557536a52b4d9bf0bb
lucho20091 <124323772+lucho20091@users.noreply.github.com>
2025-10-27 18:32:10 -0500

    add find create update delete

---
+++ prisma/migrations/20251027231911_refactor/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userPreferenceId` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[userId]` on the table `UserPreference` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `userId` to the `UserPreference` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."User" DROP CONSTRAINT "User_userPreferenceId_fkey";
+
+-- DropIndex
+DROP INDEX "public"."User_userPreferenceId_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "userPreferenceId";
+
+-- AlterTable
+ALTER TABLE "UserPreference" ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserPreference_userId_key" ON "UserPreference"("userId");
+
+-- AddForeignKey
+ALTER TABLE "UserPreference" ADD CONSTRAINT "UserPreference_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- script.ts
+++ script.ts
@@ -4,8 +4,226 @@ import { PrismaClient } from "./generated/prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({ data: { name: "kyle" } });
-  console.log(user);
+  await prisma.user.deleteMany();
+  const user = await prisma.user.create({
+    data: {
+      name: "kyle",
+      email: "kyle@test.com",
+      age: 27,
+      userPreference: {
+        create: {
+          emailUpdates: true,
+        },
+      },
+    },
+    // include: {
+    //   userPreference: true,
+    // },
+    select: {
+      name: true,
+      id: true,
+      userPreference: true,
+    },
+  });
+  // we create a post using the first user created
+  const firstUserPost = await prisma.post.create({
+    data: {
+      title: "hello world",
+      authorId: user.id,
+      averageRating: 2,
+    },
+  });
+  // update first user
+  const update = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      name: "felipe",
+    },
+  });
+
+  const updateNumberDinamically = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      age: {
+        // increment: 1,
+        // decrement: 1,
+        // multiply: 10,
+        divide: 2,
+      },
+    },
+  });
+  // we could connect a user to userpreferences using the id of the using preferences
+  // and disconnect it if we dont need that anymore
+  // first create userPreference with no userId and then later on associate it using connect property
+  // this is available in create and update
+  // it may be useful if we would like to preserve data but id not be associated to the user anymore
+  // not sure what a real case can be unless to store data for the sake of it
+  const updateEmailUpdates = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      userPreference: {
+        update: {
+          emailUpdates: false,
+        },
+      },
+    },
+  });
+  const users = await prisma.user.createMany({
+    data: [
+      {
+        name: "kyle",
+        email: "kyle2@test.com",
+        age: 50,
+      },
+      {
+        name: "kyle",
+        email: "kyle3@test.com",
+        age: 22,
+      },
+      {
+        name: "peter",
+        email: "pete@test.com",
+        age: 27,
+      },
+    ],
+  });
+  const updateMany = await prisma.user.updateMany({
+    where: {
+      name: "kyle",
+    }, // <- Add this comma here
+    data: {
+      name: "sara",
+    },
+  });
+
+  const deleteUser = await prisma.user.delete({
+    where: {
+      id: user.id,
+    },
+  });
+  // find unique only works for attributes that are unique
+  const findUser = await prisma.user.findUnique({
+    where: {
+      email: "kyle2@test.com",
+    },
+    select: {
+      name: true,
+      email: true,
+      age: true,
+    },
+  });
+
+  const findFirst = await prisma.user.findFirst({
+    where: {
+      name: "kyle",
+    },
+  });
+
+  const findMany = await prisma.user.findMany({
+    where: {
+      name: "kyle",
+    },
+  });
+
+  const findManyPagination = await prisma.user.findMany({
+    where: {
+      name: "kyle",
+    },
+    orderBy: {
+      age: "desc",
+      // age: "asc",
+    },
+    take: 2,
+    skip: 2,
+  });
+
+  const findManyNotEqual = await prisma.user.findMany({
+    where: {
+      name: { not: "kyle" },
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+
+  const findManyInArray = await prisma.user.findMany({
+    where: {
+      age: {
+        // in: [22, 27],
+        // oposite is
+        notIn: [22, 27],
+      },
+    },
+  });
+
+  const findManyOther = await prisma.user.findMany({
+    where: {
+      // name: "kyle",
+      age: {
+        // lte: 30,
+        // lt: 30,
+        // gt: 30,
+        gte: 30,
+      },
+      email: {
+        contains: "@test.com",
+        startsWith: "kyle",
+        endsWith: "@test.com",
+      },
+    },
+  });
+
+  const findManyAndOrNot = await prisma.user.findMany({
+    where: {
+      // AND: [{ email: { startsWith: "kyle" } }, { name: "kyle" }],
+      // OR: [{ email: { startsWith: "kyle" } }, { email: { endsWith: "kyle" } }],
+      NOT: { email: { startsWith: "kyle" } },
+    },
+  });
+
+  const findManyRelations = await prisma.user.findMany({
+    where: {
+      writtenPosts: {
+        every: {
+          title: { contains: "hello world" },
+        },
+      },
+    },
+  });
+
+  const findPost = await prisma.post.findMany({
+    where: {
+      author: {
+        is: {
+          age: 27,
+        },
+      },
+    },
+  });
+  // console.log(user);
+  // console.log(users);
+  // console.log(findUser);
+  // console.log(findFirst);
+  // console.log(findMany);
+  // console.log(findManyPagination);
+  // console.log(findManyNotEqual);
+  // console.log(findManyInArray);
+  // console.log(findManyOther);
+  // console.log(findManyAndOrNot);
+  // console.log(findManyRelations);
+  // console.log(findPost);
+  // console.log(update);
+  // console.log(updateMany);
+  // console.log(updateEmailUpdates);
+  // console.log(updateNumberDinamically);
+  console.log(deleteUser);
 }
 
 main()
